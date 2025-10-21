750d91f73a8b89a8c1591bc69da4085863b9cbba
Legna234 <103796076+Legna234@users.noreply.github.com>
2025-10-21 19:15:49 +0000

    --Chat local funcional

--- chat-beta/server/index.js
+++ chat-beta/server/index.js
@@ -1,9 +1,35 @@
 import express from 'express'
-const port = process.env.PORT ?? 3000
+import logger from 'morgan'
+
+import {Server} from 'socket.io'
+import {createServer} from 'node:http'
+import { Socket } from 'node:dgram'
+import { on } from 'node:events'
+
+const port = process.env.PORT ?? 3003
+
 const app = express()
-app.get('/', (req, res) => {
-    res.send('<h1>Esto es el chat<h1>')
+const server = createServer(app)
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user has connected!')
+
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected!')
+  })
+
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message', msg)
+  })
 })
-app.listen(port, () => {
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/client/index.html')
+})
+server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
